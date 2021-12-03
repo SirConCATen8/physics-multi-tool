a3beee9197d4d3fd4a37b412aa805b35acd6f3c0
SirConCATen8 <92690146+SirConCATen8@users.noreply.github.com>
2021-12-03 17:49:05 -0500

    Update PhysicsFunctions.php
    All return arrays that include units for the calculations.

--- PhysicsFunctions.php
+++ PhysicsFunctions.php
@@ -4,34 +4,34 @@
 function velocity_classic($distance, $time){
     $unit = "m/s";
     $velocity = $distance/$time;
-    return $velocity;
+    return [$velocity, $unit];
 }
 
 //Classic formula for determining acceleration
 function acceleration_classic($velocity_delta, $time){
     $unit = "m/s<sup>2</sup>";
     $acceleration = $velocity_delta/$time;
-    return $acceleration;
+    return [$acceleration, $unit];
 }
 
 //Classic formula for determining density
 function density_classic($mass,$volume){
     $unit = "g/L";
     $density = $mass/$volume;
-    return $density;
+    return [$density, $unit];
 }
 
 //Classic formula for determining force
 function force_classic($mass, $acceleration){
     $unit = "N";
     $force = $mass*$acceleration;
-    return $force;
+    return [$force, $unit];
 }
 
 function work_classic($force, $distance){
     $unit = "J";
     $work = $force*$distance;
-    return $work;
+    return [$work, $unit];
 }
 
 ?>
